d502a27919851d3c83d009b9c755347ac878f44d
helloelio <70343671+helloelio@users.noreply.github.com>
2021-03-10 11:44:38 +0300

    master

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const body = document.querySelector("body");
 const btn = document.querySelector(".bmbtn");
 const burgerMenu = document.querySelector(".burger-menu");
 const nav = document.querySelector(".nav");
+const navItems = document.querySelector(".nav__items");
 
 console.log(btn);
 
 btn.addEventListener("click", function () {
   burgerMenu.classList.toggle("burger-menu-open");
   body.classList.toggle("scroll-hidden");
+  nav.classList.toggle("nav-in-burger");
+  nav.classList.toggle("nav");
+  navItems.classList.toggle("nav-items-in-burger");
+  navItems.classList.toggle("nav__items");
 });
-
-if (burgerMenu.classList === "burger-menu-open") {
-  nav.style.dispaly = "flex";
-}
