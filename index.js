27f84d0e4a2ee8612427cd396df29c72304e26e1
helloelio <70343671+helloelio@users.noreply.github.com>
2021-03-10 20:15:33 +0300

    modified: index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const body = document.querySelector("body");
-const btn = document.querySelector(".bmbtn");
-const burgerMenu = document.querySelector(".burger-menu");
-const nav = document.querySelector(".nav");
-const navItems = document.querySelector(".nav__items");
+const body = document.querySelector('body');
+const btn = document.querySelector('.bmbtn');
+const burgerMenu = document.querySelector('.burger-menu');
+const nav = document.querySelector('.nav');
+const navItems = document.querySelector('.nav__items');
 
-console.log(btn);
-
-btn.addEventListener("click", function () {
-  burgerMenu.classList.toggle("burger-menu-open");
-  body.classList.toggle("scroll-hidden");
-  nav.classList.toggle("nav-in-burger");
-  nav.classList.toggle("nav");
-  navItems.classList.toggle("nav-items-in-burger");
-  navItems.classList.toggle("nav__items");
+btn.addEventListener('click', function () {
+  burgerMenu.classList.toggle('burger-menu-open');
+  body.classList.toggle('scroll-hidden');
+  nav.classList.toggle('nav-in-burger');
+  nav.classList.toggle('nav');
+  navItems.classList.toggle('nav-items-in-burger');
+  navItems.classList.toggle('nav__items');
 });
